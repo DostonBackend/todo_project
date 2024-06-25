0a80bed9e2ac7bc02ea7358eebad148e31ea6ed2
DostonBackend <nurillayevdoston99@gmail.com>
2024-06-25 17:39:50 +0500

    main py added

---
+++ main.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    primes = []
+    x = int(input("Enter a number: "))
+    for i in range(2, x + 1):
+        is_prime = True
+        for j in range(2, i):
+            if i % j == 0:
+                is_prime = False
+        if is_prime:
+            primes.append(i)
+
+    print(*primes)
